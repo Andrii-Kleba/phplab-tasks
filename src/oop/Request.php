81f53c09ca96fb332273cfe7f3afd5f98db264d6
Andrii Kleba <console44by@gmail.com>
2020-08-14 19:49:43 +0300

    create interface RequestView

--- src/oop/Request.php
+++ src/oop/Request.php
@@ -1,6 +1,58 @@
 <?php
 
-class Request
+interface RequestView
 {
 
+    public function query($key, $default = null);
+
+    public function post($key, $default = null);
+
+    public function get($key, $default = null);
+
+    public function all($key, $only = []);
+
+    public function has($key);
+
+    public function ip();
+
+    public function userAgent();
+}
+
+class Request implements RequestView
+{
+
+    public function query($key, $default = null)
+    {
+        // TODO: Implement query() method.
+    }
+
+    public function post($key, $default = null)
+    {
+        // TODO: Implement post() method.
+    }
+
+    public function get($key, $default = null)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function all($key, $only = [])
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function has($key)
+    {
+        // TODO: Implement has() method.
+    }
+
+    public function ip()
+    {
+        // TODO: Implement ip() method.
+    }
+
+    public function userAgent()
+    {
+        // TODO: Implement userAgent() method.
+    }
 }

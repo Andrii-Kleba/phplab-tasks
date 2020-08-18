7eebdce4992f791e31b0e581d2129b1fa66cb107
Andrii Kleba <console44by@gmail.com>
2020-08-18 10:50:54 +0300

    Some logic to Request class

--- src/oop/Request.php
+++ src/oop/Request.php
@@ -1,58 +1,23 @@
 <?php
 
-interface RequestView
+class Request
 {
+    public array $request;
+    public array $getRequest;
 
-    public function query($key, $default = null);
-
-    public function post($key, $default = null);
-
-    public function get($key, $default = null);
-
-    public function all($key, $only = []);
-
-    public function has($key);
-
-    public function ip();
-
-    public function userAgent();
-}
-
-class Request implements RequestView
-{
+    public function __construct(array $request, array $getRequest)
+    {
+        $this->request = $request;
+        $this->getRequest = $getRequest;
+    }
 
     public function query($key, $default = null)
     {
-        // TODO: Implement query() method.
+        return !empty($_GET[$key]) ? $_GET[$key] : $_GET[$default];
     }
 
     public function post($key, $default = null)
     {
-        // TODO: Implement post() method.
-    }
-
-    public function get($key, $default = null)
-    {
-        // TODO: Implement get() method.
-    }
-
-    public function all($key, $only = [])
-    {
-        // TODO: Implement all() method.
-    }
-
-    public function has($key)
-    {
-        // TODO: Implement has() method.
-    }
-
-    public function ip()
-    {
-        // TODO: Implement ip() method.
-    }
-
-    public function userAgent()
-    {
-        // TODO: Implement userAgent() method.
+        return !empty($_POST[$key]) ? $_POST[$key] : $_POST[$default];
     }
 }
